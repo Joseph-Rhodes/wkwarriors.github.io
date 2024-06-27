4b0c50f48e9064569b255a92de39bb979f764c31
Joseph-Rhodes <josephlrhodes2021@gmail.com>
2024-06-27 14:58:02 -0400

    About Us
    Need to fix minor things bu overall ocmplete

--- wkwarriors-website/src/Components/Homepage/AboutUs.js
+++ wkwarriors-website/src/Components/Homepage/AboutUs.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './AboutUs.css';
+import FoundationImage from '../../Images/teamPhoto.png'; // Ensure the path and file name are correct
+import { Link } from 'react-router-dom';
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className="about-us-container">
+      <div className="image-overlay-container">
+        <img src={FoundationImage} alt="USA Basketball Foundation" className="foundation-image" />
+        <div className="image-overlay"></div>
+      </div>
+      <div className="about-us-content">
+        <h1>West Kirby Warriors</h1>
+        <p>
+          The West Kirby Warriors, founded in 2001 in Hoylake, Liverpool, UK, have emerged as a promising club on the rise. 
+          With recent efforts focused on rebuilding and revitalization, they are gaining recognition for their dedication to 
+          fostering talent and community engagement in the local sports scene.
+        </p>
+        <div className="button-container2">
+          <Link to="/roster" style={{ textDecoration: 'none' }} className="meet-the-team-button">Meet the Team</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- wkwarriors-website/src/Components/Homepage/UpcomingSchedule.js
+++ wkwarriors-website/src/Components/Homepage/UpcomingSchedule.js
@@ -5,28 +5,54 @@ import "./UpcomingSchedule.css";
 const UpcomingSchedule = () => {
   return (
     <>
-      <div className="upcoming-schedule">
-        <h2 className="title">Upcoming Games</h2>
-        <div className="games-list">
-          <div className="game-item">
-            <Link to="/woolstonwizardsj21" style={{ textDecoration: 'none' }} className="game-name">Game vs. Woolston Wizards</Link>
-            <p>Warrington Summer League</p>
-            <p>Fri, Jun 21 | Liverpool, UK</p>
-          </div>
-          <div className="game-item">
-            <Link to="/woolstonwizardsj21" style={{ textDecoration: 'none' }} className="game-name">Game vs. Woolston Wizards</Link>
-            <p>Warrington Summer League</p>
-            <p>Fri, Jun 21 | Liverpool, UK</p>
-          </div>
-          <div className="game-item">
-            <Link to="/woolstonwizardsj21" style={{ textDecoration: 'none' }} className="game-name">Game vs. Woolston Wizards</Link>
-            <p>Warrington Summer League</p>
-            <p>Fri, Jun 21 | Liverpool, UK</p>
+      <div className="upcoming-schedule-container">
+        <div className="upcoming-schedule">
+          <h2 className="title">Upcoming Games</h2>
+          <div className="games-list">
+            <div className="game-item">
+              <Link
+                to="/woolstonwizardsj21"
+                style={{ textDecoration: "none" }}
+                className="game-name"
+              >
+                Game vs. Woolston Wizards
+              </Link>
+              <p>Warrington Summer League</p>
+              <p>Fri, Jun 21 | Liverpool, UK</p>
+            </div>
+            <div className="game-item">
+              <Link
+                to="/woolstonwizardsj21"
+                style={{ textDecoration: "none" }}
+                className="game-name"
+              >
+                Game vs. Woolston Wizards
+              </Link>
+              <p>Warrington Summer League</p>
+              <p>Fri, Jun 21 | Liverpool, UK</p>
+            </div>
+            <div className="game-item">
+              <Link
+                to="/woolstonwizardsj21"
+                style={{ textDecoration: "none" }}
+                className="game-name"
+              >
+                Game vs. Woolston Wizards
+              </Link>
+              <p>Warrington Summer League</p>
+              <p>Fri, Jun 21 | Liverpool, UK</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="button-container">
-        <Link to="/schedule" style={{ textDecoration: 'none' }} className="schedule-button">See More</Link>
+        <div className="button-container">
+          <Link
+            to="/schedule"
+            style={{ textDecoration: "none" }}
+            className="schedule-button"
+          >
+            See More
+          </Link>
+        </div>
       </div>
     </>
   );
