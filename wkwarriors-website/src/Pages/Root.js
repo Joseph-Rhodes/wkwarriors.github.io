c53da79b291d30fb7f6b4a83015ffd552f9b0ee7
Joseph-Rhodes <josephlrhodes2021@gmail.com>
2024-07-02 08:17:24 -0400

    Schedule done i think

--- wkwarriors-website/src/Pages/Root.js
+++ wkwarriors-website/src/Pages/Root.js
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import useScrollToTop from "../Components/ScrollToTop";
 
 export default function Root() {
+  useScrollToTop();
+
   return (
     <div>
       <Header />
--- wkwarriors-website/src/Pages/Schedule.js
+++ wkwarriors-website/src/Pages/Schedule.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import SchedulePage from '../Components/SchedulePage'
 
 const Schedule = () => {
   return (
-    <div>Schedule</div>
+    <SchedulePage />
   )
 }
 
