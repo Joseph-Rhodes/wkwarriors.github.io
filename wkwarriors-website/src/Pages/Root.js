10f5675cba07dd200f2b17e9320b7ff2b5b750b0
Joseph-Rhodes <josephlrhodes2021@gmail.com>
2025-01-06 01:05:22 -0800

    restart

--- wkwarriors-website/src/Pages/Root.js
+++ wkwarriors-website/src/Pages/Root.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import useScrollToTop from "../Components/ScrollToTop";
-import { Analytics } from "@vercel/analytics/react";
+
 
 export default function Root() {
   useScrollToTop();
@@ -13,9 +13,7 @@ export default function Root() {
       <div className="content">
         <Outlet />
       </div>
-      <Footer />
-
-      <Analytics />    
+      <Footer /> 
     </div>
   );
 }
